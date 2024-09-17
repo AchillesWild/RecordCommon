30eb1c6048a03765e359aea6d26eea4c130cce76
AchillesWild <56060020+AchillesWild@users.noreply.github.com>
2024-09-18 01:20:21 +0800

    log_model_click

--- src/main/java/com/achilles/record/enums/ClzConstant.java
+++ src/main/java/com/achilles/record/enums/ClzConstant.java
@@ -2,5 +2,7 @@ package com.achilles.record.enums;
 
 public class ClzConstant {
 
-    public static final String MISSION = "com.achilles.server.controller.mission.MissionCommandController";
+    public static final String MISSION_COMMAND = "com.achilles.server.controller.mission.MissionCommandController";
+
+    public static final String MISSION_QUERY= "com.achilles.server.controller.mission.MissionQryController";
 }
--- src/main/java/com/achilles/record/enums/ModelFunctionEnum.java
+++ src/main/java/com/achilles/record/enums/ModelFunctionEnum.java
@@ -5,20 +5,20 @@ import java.util.*;
 public enum ModelFunctionEnum {
 
     //--------------------------------------------- mission  start ----------------------------------------------------------------
-    MISSION_ADD(ClzConstant.MISSION + ".add", ModelEnum.MISSION.getCode(),"add", ModelEnum.MISSION.getValue() + "_新增"),
-//    MISSION_DEL("com.achilles.server.controller.mission.MissionCommandController.delete",501, "待办_删除"),
-//    MISSION_UPDATE("com.achilles.server.controller.mission.MissionCommandController.update",502, "待办_修改"),
-//    MISSION_UPDATE_INITIAL("com.achilles.server.controller.mission.MissionCommandController.updateInitial",503, "待办_改为未处理"),
-//    MISSION_UPDATE_DOING("com.achilles.server.controller.mission.MissionCommandController.updateDoing",504, "待办_改为处理中"),
-//    MISSION_UPDATE_DONE("com.achilles.server.controller.mission.MissionCommandController.updateDone",505, "待办_改为已处理"),
-//
-//    MISSION_DOWNLOAD("com.achilles.server.controller.mission.MissionQryController.download",506, "待办_下载"),
-//    MISSION_GET("com.achilles.server.controller.mission.MissionQryController.get",507, "待办_根据id查询"),
-//    MISSION_LIST("com.achilles.server.controller.mission.MissionQryController.list",508, "待办_查询列表"),
-//    MISSION_COUNT("com.achilles.server.controller.mission.MissionQryController.count",509, "待办_查询总数"),
-//    MISSION_TYPE("com.achilles.server.controller.mission.MissionQryController.type",510, "待办_查询所有类型"),
-//    MISSION_PACE("com.achilles.server.controller.mission.MissionQryController.pace",511, "待办_查询所有进度"),
-//    MISSION_GET_IMG("com.achilles.server.controller.mission.MissionQryController.getBigImg",512, "待办_获取大图"),
+    MISSION_ADD(ClzConstant.MISSION_COMMAND + ".add", ModelEnum.MISSION.getCode(),"add", ModelEnum.MISSION.getValue() + "_新增"),
+    MISSION_DEL(ClzConstant.MISSION_COMMAND + ".delete",ModelEnum.MISSION.getCode(),"delete", ModelEnum.MISSION.getValue() + "_删除"),
+    MISSION_UPDATE(ClzConstant.MISSION_COMMAND + ".update",ModelEnum.MISSION.getCode(),"update", ModelEnum.MISSION.getValue() + "_修改"),
+    MISSION_UPDATE_INITIAL(ClzConstant.MISSION_COMMAND + ".updateInitial",ModelEnum.MISSION.getCode(),"updateInitial", ModelEnum.MISSION.getValue() + "_改为未处理"),
+    MISSION_UPDATE_DOING(ClzConstant.MISSION_COMMAND + ".updateDoing", ModelEnum.MISSION.getCode(),"updateDoing", ModelEnum.MISSION.getValue() + "_改为处理中"),
+    MISSION_UPDATE_DONE(ClzConstant.MISSION_COMMAND + ".updateDone",ModelEnum.MISSION.getCode(),"updateDone", ModelEnum.MISSION.getValue() + "_改为已处理"),
+
+    MISSION_DOWNLOAD(ClzConstant.MISSION_QUERY + ".download",ModelEnum.MISSION.getCode(),"download", ModelEnum.MISSION.getValue() + "_下载"),
+    MISSION_GET(ClzConstant.MISSION_QUERY + ".get", ModelEnum.MISSION.getCode(),"get", ModelEnum.MISSION.getValue() + "_根据id查询"),
+    MISSION_LIST(ClzConstant.MISSION_QUERY + ".getPageList", ModelEnum.MISSION.getCode(),"list", ModelEnum.MISSION.getValue() + "_查询列表"),
+    MISSION_COUNT(ClzConstant.MISSION_QUERY + ".getCount", ModelEnum.MISSION.getCode(),"count", ModelEnum.MISSION.getValue() + "_查询总数"),
+    MISSION_TYPE(ClzConstant.MISSION_QUERY + ".getType", ModelEnum.MISSION.getCode(),"type", ModelEnum.MISSION.getValue() + "_查询所有类型"),
+    MISSION_PACE(ClzConstant.MISSION_QUERY + ".getPace", ModelEnum.MISSION.getCode(),"pace", ModelEnum.MISSION.getValue() + "_查询所有进度"),
+    MISSION_GET_IMG(ClzConstant.MISSION_QUERY + ".getBigImg", ModelEnum.MISSION.getCode(),"getBigImg", ModelEnum.MISSION.getValue() + "_获取大图"),
     //--------------------------------------------- diary  start ----------------------------------------------------------------
 
 
