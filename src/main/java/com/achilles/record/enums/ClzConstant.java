b8a24c7270fc18a021d904a542b7fc3b390640af
AchillesWild <56060020+AchillesWild@users.noreply.github.com>
2024-09-18 11:24:50 +0800

    note

--- src/main/java/com/achilles/record/enums/ClzConstant.java
+++ src/main/java/com/achilles/record/enums/ClzConstant.java
@@ -3,6 +3,8 @@ package com.achilles.record.enums;
 public class ClzConstant {
 
     public static final String MISSION_COMMAND = "com.achilles.server.controller.mission.MissionCommandController";
-
     public static final String MISSION_QUERY= "com.achilles.server.controller.mission.MissionQryController";
+
+    public static final String NOTE_COMMAND = "com.achilles.server.controller.note.NoteCommandController";
+    public static final String NOTE_QUERY= "com.achilles.server.controller.note.NoteQryController";
 }
--- src/main/java/com/achilles/record/enums/ModelFunctionEnum.java
+++ src/main/java/com/achilles/record/enums/ModelFunctionEnum.java
@@ -6,22 +6,33 @@ public enum ModelFunctionEnum {
 
     //--------------------------------------------- mission  start ----------------------------------------------------------------
     MISSION_ADD(ClzConstant.MISSION_COMMAND + ".add", ModelEnum.MISSION.getCode(),"add", ModelEnum.MISSION.getValue() + "_新增"),
-    MISSION_DEL(ClzConstant.MISSION_COMMAND + ".delete",ModelEnum.MISSION.getCode(),"delete", ModelEnum.MISSION.getValue() + "_删除"),
-    MISSION_UPDATE(ClzConstant.MISSION_COMMAND + ".update",ModelEnum.MISSION.getCode(),"update", ModelEnum.MISSION.getValue() + "_修改"),
-    MISSION_UPDATE_INITIAL(ClzConstant.MISSION_COMMAND + ".updateInitial",ModelEnum.MISSION.getCode(),"updateInitial", ModelEnum.MISSION.getValue() + "_改为未处理"),
+    MISSION_DEL(ClzConstant.MISSION_COMMAND + ".delete", ModelEnum.MISSION.getCode(),"delete", ModelEnum.MISSION.getValue() + "_删除"),
+    MISSION_UPDATE(ClzConstant.MISSION_COMMAND + ".update", ModelEnum.MISSION.getCode(),"update", ModelEnum.MISSION.getValue() + "_修改"),
+    MISSION_UPDATE_INITIAL(ClzConstant.MISSION_COMMAND + ".updateInitial", ModelEnum.MISSION.getCode(),"updateInitial", ModelEnum.MISSION.getValue() + "_改为未处理"),
     MISSION_UPDATE_DOING(ClzConstant.MISSION_COMMAND + ".updateDoing", ModelEnum.MISSION.getCode(),"updateDoing", ModelEnum.MISSION.getValue() + "_改为处理中"),
-    MISSION_UPDATE_DONE(ClzConstant.MISSION_COMMAND + ".updateDone",ModelEnum.MISSION.getCode(),"updateDone", ModelEnum.MISSION.getValue() + "_改为已处理"),
+    MISSION_UPDATE_DONE(ClzConstant.MISSION_COMMAND + ".updateDone", ModelEnum.MISSION.getCode(),"updateDone", ModelEnum.MISSION.getValue() + "_改为已处理"),
 
     MISSION_DOWNLOAD(ClzConstant.MISSION_QUERY + ".download",ModelEnum.MISSION.getCode(),"download", ModelEnum.MISSION.getValue() + "_下载"),
     MISSION_GET(ClzConstant.MISSION_QUERY + ".get", ModelEnum.MISSION.getCode(),"get", ModelEnum.MISSION.getValue() + "_根据id查询"),
-    MISSION_LIST(ClzConstant.MISSION_QUERY + ".getPageList", ModelEnum.MISSION.getCode(),"list", ModelEnum.MISSION.getValue() + "_查询列表"),
-    MISSION_COUNT(ClzConstant.MISSION_QUERY + ".getCount", ModelEnum.MISSION.getCode(),"count", ModelEnum.MISSION.getValue() + "_查询总数"),
-    MISSION_TYPE(ClzConstant.MISSION_QUERY + ".getType", ModelEnum.MISSION.getCode(),"type", ModelEnum.MISSION.getValue() + "_查询所有类型"),
-    MISSION_PACE(ClzConstant.MISSION_QUERY + ".getPace", ModelEnum.MISSION.getCode(),"pace", ModelEnum.MISSION.getValue() + "_查询所有进度"),
+    MISSION_LIST(ClzConstant.MISSION_QUERY + ".getPageList", ModelEnum.MISSION.getCode(),"getPageList", ModelEnum.MISSION.getValue() + "_查询列表"),
+    MISSION_COUNT(ClzConstant.MISSION_QUERY + ".getCount", ModelEnum.MISSION.getCode(),"getCount", ModelEnum.MISSION.getValue() + "_查询总数"),
+    MISSION_TYPE(ClzConstant.MISSION_QUERY + ".getType", ModelEnum.MISSION.getCode(),"getType", ModelEnum.MISSION.getValue() + "_查询所有类型"),
+    MISSION_PACE(ClzConstant.MISSION_QUERY + ".getPace", ModelEnum.MISSION.getCode(),"getPace", ModelEnum.MISSION.getValue() + "_查询所有进度"),
     MISSION_GET_IMG(ClzConstant.MISSION_QUERY + ".getBigImg", ModelEnum.MISSION.getCode(),"getBigImg", ModelEnum.MISSION.getValue() + "_获取大图"),
+
     //--------------------------------------------- diary  start ----------------------------------------------------------------
+    NOTE_ADD(ClzConstant.NOTE_COMMAND + ".add", ModelEnum.NOTE.getCode(),"add", ModelEnum.NOTE.getValue() + "_新增"),
+    NOTE_DEL(ClzConstant.NOTE_COMMAND + ".delete", ModelEnum.NOTE.getCode(),"delete", ModelEnum.NOTE.getValue() + "_删除"),
+    NOTE_UPDATE(ClzConstant.NOTE_COMMAND + ".update", ModelEnum.NOTE.getCode(),"update", ModelEnum.NOTE.getValue() + "_修改"),
+    NOTE_COPY(ClzConstant.NOTE_COMMAND + ".copy", ModelEnum.NOTE.getCode(),"copy", ModelEnum.NOTE.getValue() + "_复制"),
+    NOTE_UPDATE_CONTENT(ClzConstant.NOTE_COMMAND + ".updateContent", ModelEnum.NOTE.getCode(),"updateContent", ModelEnum.NOTE.getValue() + "_修改正文"),
 
-
+    NOTE_DOWNLOAD(ClzConstant.NOTE_QUERY + ".download",ModelEnum.NOTE.getCode(),"download", ModelEnum.NOTE.getValue() + "_下载"),
+    NOTE_GET(ClzConstant.NOTE_QUERY + ".get", ModelEnum.NOTE.getCode(),"get", ModelEnum.NOTE.getValue() + "_根据id查询"),
+    NOTE_LIST(ClzConstant.NOTE_QUERY + ".getPageList", ModelEnum.NOTE.getCode(),"getPageList", ModelEnum.NOTE.getValue() + "_查询列表"),
+    NOTE_COUNT(ClzConstant.NOTE_QUERY + ".getCount", ModelEnum.NOTE.getCode(),"getCount", ModelEnum.NOTE.getValue() + "_查询总数"),
+    NOTE_TYPE(ClzConstant.NOTE_QUERY + ".getType", ModelEnum.NOTE.getCode(),"getType", ModelEnum.NOTE.getValue() + "_查询所有类型"),
+    NOTE_GET_IMG(ClzConstant.NOTE_QUERY + ".getBigImg", ModelEnum.NOTE.getCode(),"getBigImg", ModelEnum.NOTE.getValue() + "_获取大图"),
     ;
 
     private String method;
