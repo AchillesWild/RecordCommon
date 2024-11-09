07d8543cad4961b0ff609b9588a130ae3a33f1af
AchillesWild <56060020+AchillesWild@users.noreply.github.com>
2024-11-09 22:42:39 +0800

    配置

---
+++ src/main/java/com/achilles/record/constant/ConfigConstant.java
@@ -0,0 +1,10 @@
+package com.achilles.record.constant;
+
+public class ConfigConstant {
+
+    public static final String CONFIGS = "configs";
+
+    public static final String AUTH_CODE = "authCode";
+
+    public static final String CLICK_LOG_SWITCH = "clickLogSwitch";
+}
