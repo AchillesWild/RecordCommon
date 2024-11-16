ef15778c16b3ffee213b35ebd63cd55bb4810063
AchillesWild <56060020+AchillesWild@users.noreply.github.com>
2024-11-17 00:21:03 +0800

    traceid  add date

--- src/main/java/com/achilles/record/enums/ModelEnum.java
+++ src/main/java/com/achilles/record/enums/ModelEnum.java
@@ -6,59 +6,69 @@ import java.util.Map;
 
 public enum ModelEnum {
 
-    MISSION("mission", "待办"),
+    MISSION("mission", "待办", 0),
 
-    NOTE("note", "笔记"),
+    NOTE("note", "笔记", 1),
 
-    FINANCE_TRANSACTION_TYPE("finance_transaction_type","收/支类型"),
-    FINANCE("finance", "账务"),
-    FINANCE_MONTH("finance_month","账务-月度"),
 
-    PRODUCT("product", "计量物"),
-    PRODUCT_OUTPUT("product_output","计量"),
-    PRODUCT_OUTPUT_MONTH("product_output_month","计量-月度"),
+    FINANCE("finance", "账务", 2),
+    FINANCE_MONTH("finance_month","账务-月度", 3),
+    FINANCE_TRANSACTION_TYPE("finance_transaction_type","收/支类型", 4),
 
-    SUPPLIER("supplier","供应商"),
-    GOODS( "goods","商品"),
-    GOODS_TRADE("goods_trade","买/卖"),
-    GOODS_TRADE_MONTH("goods_trade_month","买/卖-月度"),
+    PRODUCT_OUTPUT("product_output","计量", 5),
+    PRODUCT_OUTPUT_MONTH("product_output_month","计量-月度", 6),
+    PRODUCT("product", "计量物", 7),
 
-    SHARE_AREA("share_area", "信息板"),
+    GOODS_TRADE("goods_trade","买/卖", 8),
+    GOODS_TRADE_MONTH("goods_trade_month","买/卖-月度", 9),
+    SUPPLIER("supplier","供应商", 10),
+    GOODS( "goods","商品", 11),
 
-    SHARE_USER("share_user", "分享用户"),
+    SHARE_AREA("share_area", "信息板", 12),
 
-    USER("user", "用户"),
+    SHARE_USER("share_user", "分享用户", 13),
 
-    USER_NOTICE("user_notice", "用户通知"),
+    USER("user", "用户", 14),
+
+    USER_NOTICE("user_notice", "用户通知", 15),
 
     ;
 
-    private String code;
+    private String key;
 
     private String value;
 
-    ModelEnum(String code, String value) {
-        this.code = code;
+    private Integer sort;
+
+    ModelEnum(String key, String value, Integer sort) {
+        this.key = key;
         this.value = value;
+        this.sort = sort;
     }
 
     public String getValue() {
         return value;
     }
 
-    public String getCode() {
-        return code;
+    public String getKey() {
+        return key;
+    }
+
+    public Integer getSort() {
+        return sort;
     }
 
     public static String getValue(String code) {
-        return map.get(code);
+        return keyValueMap.get(code);
     }
 
-    public static Map<String, String> map = new HashMap<>();
+    public static Map<String, String> keyValueMap = new HashMap<>();
+    public static Map<String, Integer> keySortMap = new HashMap<>();
 
     static {
         for (ModelEnum modelEnum : EnumSet.allOf(ModelEnum.class)) {
-            map.put(modelEnum.getCode(), modelEnum.getValue());
+            keyValueMap.put(modelEnum.getKey(), modelEnum.getValue());
+            keySortMap.put(modelEnum.getKey(), modelEnum.getSort());
         }
     }
 
@@ -68,7 +78,7 @@ public enum ModelEnum {
             return false;
         }
 
-        String value = map.get(code);
+        String value = keyValueMap.get(code);
         if(value != null) {
             return true;
         }
