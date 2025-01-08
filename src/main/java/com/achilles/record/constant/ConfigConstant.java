c7ab2e497ff3538eaa04307f701e60c8a5df40eb
AchillesWild <56060020+AchillesWild@users.noreply.github.com>
2025-01-08 11:18:30 +0800

    configCache.put(code, val);

--- src/main/java/com/achilles/record/constant/ConfigConstant.java
+++ src/main/java/com/achilles/record/constant/ConfigConstant.java
@@ -2,8 +2,6 @@ package com.achilles.record.constant;
 
 public class ConfigConstant {
 
-    public static final String CONFIGS = "configs";
-
     public static final String AUTH_CODE = "authCode";
 
     public static final String CLICK_LOG_SWITCH = "clickLogSwitch";
