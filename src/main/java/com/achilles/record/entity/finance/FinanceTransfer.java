15237a3d1327429feb99da53b9a0a235ef931e10
Achilles <532398999@qq.com>
2026-04-13 18:56:25 +0800

    add FinanceTransfer

---
+++ src/main/java/com/achilles/record/entity/finance/FinanceTransfer.java
@@ -0,0 +1,27 @@
+package com.achilles.record.entity.finance;
+
+import com.achilles.model.entity.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FinanceTransfer extends BaseEntity {
+
+    private Long amount;
+
+    private Integer flowType;
+
+    private String object;
+
+    private Integer tradeDate;
+
+    private Integer hasImg;
+
+    private String compressImgUrl;
+
+    private String imgUrl;
+
+    private String remark;
+
+}
