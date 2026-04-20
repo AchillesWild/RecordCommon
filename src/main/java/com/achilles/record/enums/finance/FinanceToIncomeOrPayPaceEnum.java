5e81a31612a34593087ec2909ea169da10f9b29a
Achilles <532398999@qq.com>
2026-04-21 00:48:46 +0800

    add FinanceToIncomeOrPay

---
+++ src/main/java/com/achilles/record/enums/finance/FinanceToIncomeOrPayPaceEnum.java
@@ -0,0 +1,59 @@
+package com.achilles.record.enums.finance;
+
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
+public enum FinanceToIncomeOrPayPaceEnum {
+
+
+    INITIAL(0,"待处理"),
+
+//    DOING(1,"处理中"),
+
+    DONE(2,"已完成");
+
+    private Integer key;
+    private String value;
+
+    FinanceToIncomeOrPayPaceEnum(Integer key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public Integer getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static String getValue(Integer key) {
+        return getMap().get(key);
+    }
+
+    public static Map<Integer,String> getMap() {
+        Map<Integer, String> map = new HashMap<>();
+        for (FinanceToIncomeOrPayPaceEnum financeToIncomeOrPayPaceEnum : EnumSet.allOf(FinanceToIncomeOrPayPaceEnum.class)) {
+            map.put(financeToIncomeOrPayPaceEnum.getKey(), financeToIncomeOrPayPaceEnum.getValue());
+        }
+        return map;
+    }
+
+    public static boolean contains(Integer key){
+
+        if(key == null){
+            return false;
+        }
+
+        FinanceToIncomeOrPayPaceEnum[] financeToIncomeOrPayPaceEnums = FinanceToIncomeOrPayPaceEnum.values();
+        for (FinanceToIncomeOrPayPaceEnum financeToIncomeOrPayPaceEnum : financeToIncomeOrPayPaceEnums){
+            if(financeToIncomeOrPayPaceEnum.key == key){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
